bb17b61cdc7d105d77d512ec3be11bb84e331afd
Rodrigo Ortiz <roloortizgarcia@gmail.com>
2019-01-04 13:40:02 +0100

    Advanced all routes, added background and stylized. close to finishing

--- client/src/app/routes.ts
+++ client/src/app/routes.ts
@@ -3,11 +3,17 @@ import {Routes} from '@angular/router';
 
 //components
 import {HomeComponent} from './components/home/home.component';
+import { VinosComponent } from './components/vinos/vinos.component';
+import { AceitesComponent } from './components/aceites/aceites.component';
+import { AboutUsComponent } from './components/about-us/about-us.component';
 
 export const routes: Routes = [
     
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home',  component: HomeComponent},
+  { path: 'vinos',  component: VinosComponent},
+  { path: 'aceites',  component: AceitesComponent},
+  { path: 'contacto',  component: AboutUsComponent},
   
 ]
 
